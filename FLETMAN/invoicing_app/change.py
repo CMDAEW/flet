f9b7a3eb6e99a771a39c972ba28ddfb02c7f6b65
CMDAEW <cmuede@gmail.com>
2024-10-03 11:51:50 +0200

    new

--- FLETMAN/invoicing_app/change.py
+++ FLETMAN/invoicing_app/change.py
@@ -1,7 +1,12 @@
 import csv
+import os
 
-input_file = 'FLETMAN/invoicing_app/assets/LV_FILES/EP.csv'
-output_file = 'FLETMAN/invoicing_app/assets/LV_FILES/EP_updated.csv'
+# Get the current working directory
+current_dir = os.getcwd()
+
+# Construct the paths relative to the current directory
+input_file = os.path.join(current_dir, 'assets', 'LV_FILES', 'EP.csv')
+output_file = os.path.join(current_dir, 'assets', 'LV_FILES', 'EP_updated.csv')
 
 with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', newline='', encoding='utf-8') as outfile:
     reader = csv.reader(infile, delimiter=';')
